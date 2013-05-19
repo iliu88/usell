90eab437733e0e04e824e3923c8d15448eafe240
sszuflita <sam.szuflita@gmail.com>
2013-05-18 21:32:15 -0700

    Fixed conflicts

--- model.py
+++ model.py
@@ -2,48 +2,6 @@
 # Data is stored hierarchically: all items belong to a user,
 # all users belong to a network.
 
-<<<<<<< HEAD
-from google.appengine.ext import db
-from google.appengine.ext import search
-
-class Item(search.SearchableModel):
-    # Item Name
-    itemName = db.StringProperty(required=False)
-    # Price of Item
-    price = db.StringProperty(required=False)
-    # Description of Item
-    description = db.StringProperty(required=False)
-    # Image of item
-    image = db.StringProperty(required=False)
-    # Category of item
-    category = db.StringProperty(required=False)
-    
-    # Identifies when message was posted
-    when = db.DateTimeProperty(auto_now_add=True)
-
-
-# An object representing a network (a college/university/other
-# community group if we include non-colleges)
-class Network(db.Model):
-    name = db.StringProperty()
-    # some form of token
-
-
-# An object representing a user of our application
-class User(db.Model):
-    firstName = db.StringProperty()
-    lastName = db.StringProperty()
-    # fb_id_token
-    # items in marketplace
-    # reviews
-
-    # return full name
-    def fullName(self):
-        return self.firstName + " " + self.lastName
-
-
-
-=======
 from google.appengine.ext import db, search
 
 class Item(search.SearchableModel):
@@ -73,4 +31,3 @@ class DisplayItem():
         self.sellerName = sellerName
         self.sellerURL = sellerURL
         self.description = description
->>>>>>> sam's-branch
